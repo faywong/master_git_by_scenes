29c8eadb414ed47581903741ca235cd73d64f478
faywong <philip584521@gmail.com>
2017-03-02 19:31:30 +0800

    s2:01: add some_func function

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+void some_func(void) {
+   printf("This is print output from some_func\n"); 
+}
+
 int main(void) {
     printf("I am output of test.c\n");
+    some_func();
     return 0;
 }
